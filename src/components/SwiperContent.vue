c6c4dadcf0808f482c5b3bee570f985cd5e4cd5d
Yunfen <yfqiu@thoughtworks.com>
2018-07-02 11:37:30 +0800

    style

--- src/components/SwiperContent.vue
+++ src/components/SwiperContent.vue
@@ -1,8 +1,8 @@
 <template>
-    <div>
-        <img :src= "require('../imgs/'+imgSrc)" />
-        <div>
-            <p>{{content}}</p>
+    <div class="activuty-box">
+        <img :src= "require('../imgs/'+imgSrc)" class="activity-img"/>
+        <div class="activity-footer">
+            <p class="end-time-box">{{content}}</p>
         </div>
     </div>
 </template>
@@ -21,3 +21,32 @@ export default {
     }
 }
 </script>
+
+<style>
+.activuty-box {
+    position: relative;
+    width: 345px;
+    height: 88px;
+    border-radius: 4px;
+    background-color: #d8d8d8;
+}
+.activity-img {
+    width:100%;
+    height: 100%;
+}
+.activity-footer {
+    position: absolute;
+    bottom: 0;
+    right: 0;
+    width: 100%;
+    height: 18px;
+}
+    .end-time-box {
+        margin: 0;
+        padding: 3px 11px 3px 0;
+        text-align: right;
+        font-size: 12px;
+        line-height: 12px;
+        color: #ffffff;
+    }
+</style>
