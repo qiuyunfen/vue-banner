0c17c6e19ccbffb2bf79cef8385fa0392b0aae40
Yunfen <yfqiu@thoughtworks.com>
2018-07-02 10:39:08 +0800

    frame

---
+++ src/components/Banner.vue
@@ -0,0 +1,21 @@
+<template>
+    <swiper>
+        <swiper-slide><img src="../imgs/1.jpg"/></swiper-slide>
+        <swiper-slide><img src="../imgs/2.jpg"/></swiper-slide>
+        <swiper-slide><img src="../imgs/3.jpeg"/></swiper-slide>
+    </swiper>
+</template>
+
+<script>
+import {swiper, swiperSlide} from 'vue-awesome-swiper'
+
+import 'swiper/dist/css/swiper.css'
+
+export default {
+    name: 'Banner',
+    components: {
+        swiper,
+        swiperSlide
+    }
+}
+</script>
