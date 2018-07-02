c820eb0c0fb3f84b44aa52e54a6ffcec14783699
Yunfen <yfqiu@thoughtworks.com>
2018-07-02 11:54:43 +0800

    linear-gradient

--- src/components/SwiperContent.vue
+++ src/components/SwiperContent.vue
@@ -24,27 +24,26 @@
 </script>
 
 <style>
-    .activuty-box {
-        position: relative;
-        width: 345px;
-        height: 88px;
-        border-radius: 4px;
-        background-color: #d8d8d8;
-    }
-
-    .activity-img {
-        width: 100%;
-        height: 100%;
-    }
-
-    .activity-footer {
-        position: absolute;
-        bottom: 0;
-        right: 0;
-        width: 100%;
-        height: 18px;
-    }
-
+.activuty-box {
+    position: relative;
+    width: 345px;
+    height: 88px;
+    border-radius: 4px;
+    background-color: #d8d8d8;
+}
+.activity-img {
+    width:100%;
+    height: 100%;
+}
+.activity-footer {
+    position: absolute;
+    bottom: 0;
+    right: 0;
+    width: 246px;
+    height: 18px;
+    border-radius: 5px;
+    background-image: linear-gradient(to right, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.4));;
+}
     .end-time-box {
         margin: 0;
         padding: 3px 11px 3px 0;
