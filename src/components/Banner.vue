d586fdcb68e0babf0d20d3b26b7137f0fbad4095
Yunfen <yfqiu@thoughtworks.com>
2018-07-02 10:59:48 +0800

    swiper content

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -1,6 +1,8 @@
 <template>
     <swiper>
-        <swiper-slide><img src="../imgs/1.jpg"/></swiper-slide>
+        <swiper-slide>
+            <swiper-content img-src="../imgs/1.jpg" content="截止日期2018.07.02"></swiper-content>
+        </swiper-slide>
         <swiper-slide><img src="../imgs/2.jpg"/></swiper-slide>
         <swiper-slide><img src="../imgs/3.jpeg"/></swiper-slide>
     </swiper>
@@ -8,14 +10,15 @@
 
 <script>
 import {swiper, swiperSlide} from 'vue-awesome-swiper'
-
+import SwiperContent from './SwiperContent.vue'
 import 'swiper/dist/css/swiper.css'
 
 export default {
     name: 'Banner',
     components: {
         swiper,
-        swiperSlide
+        swiperSlide,
+        SwiperContent
     }
 }
 </script>
---
+++ src/components/SwiperContent.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <img :src="imgSrc"/>
+        <div>
+            <p>{{content}}</p>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: 'SwiperContent',
+    props: {
+        imgSrc: {
+            type: String,
+            required: true
+        },
+        content: {
+            type: String,
+            required: true
+        }
+    }
+}
+</script>
